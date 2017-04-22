fea23ccc81afa7256f245624de1c9a2c86b64461
colinfwalsh <colinfwalsh@gmail.com>
2017-04-22 11:33:44 -0400

    Initialize

---
+++ hackRUProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hackRUProject
+//
+//  Created by Colin Walsh on 4/22/17.
+//  Copyright © 2017 Colin Walsh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
